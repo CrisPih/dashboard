6b76b1b42b27e2985225b7bb4cd29e335fed287d
CrisPih <152130690+CrisPih@users.noreply.github.com>
2025-12-01 19:18:36 +0000

    cambioooos

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 function App() {
-  return (
-    <Grid container spacing={5} justifyContent="center" alignItems="center">
+// const [count, setCount] = useState(0)
 
-      {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+return (
+<Grid container spacing={5} justifyContent="center" alignItems="center">
 
-      {/* Alertas */}
-      <Grid>Elemento: Alertas</Grid>
+{/* Encabezado */}
+<Grid size={{ xs: 12, md: 12 }}> <HeaderUI/> Elemento: Encabezado</Grid>
 
-      {/* Selector */}
-      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+{/* Alertas */}
+<Grid container justifyContent="right" alignItems="center">
+<AlertUI description="No se preveen lluvias"/>
 
-      {/* Indicadores */}
-      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+</Grid>
 
-      {/* Gráfico */}
-      <Grid sx={{ display: { xs: "none", md: "block" } }}>
-        Elemento: Gráfico
-      </Grid>
+{/* Selector */}
+<Grid size={{ xs: 12, md: 3 }}> <SelectorUI/></Grid>
 
-      {/* Tabla */}
-      <Grid sx={{ display: { xs: "none", md: "block" } }}>
-        Elemento: Tabla
-      </Grid>
+{/* Indicadores */}
+<Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
-    </Grid>
-  );
+{/* Gráfico */}
+<Grid
+sx={{ display: { xs: "none", md: "block"} }} >
+Elemento: Gráfico
+</Grid>
+
+{/* Tabla */}
+<Grid
+sx={{ display: { xs: "none", md: "block" } }}>
+Elemento: Tabla
+</Grid>
+
+{/* Información adicional */}
+<Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+</Grid>
+)
 }
 
-export default App;
+export default App
